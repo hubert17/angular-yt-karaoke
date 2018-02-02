540a2c70ec0ea5d2f6efb1671fb6abe6d0c0dc69
hubert17 <hewbertgabon@gmail.com>
2018-02-02 12:44:54 +0800

    added Firebase

--- src/app/components/youtube-settings.component.ts
+++ src/app/components/youtube-settings.component.ts
@@ -14,7 +14,7 @@ import { Http } from '@angular/http';
 
 export class SettingsComponent implements OnInit {
 
-    private finished = false;
+    finished = false;
     notify: any;
 
     _shared: any;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: true
+  production: true, 
+  firebaseConfig: {
+    apiKey: "AIzaSyAyw4O0GoZU_AuHKqN17_4YvR_dztOM_qY",
+    authDomain: "k-189316.firebaseapp.com",
+    databaseURL: "https://k-189316.firebaseio.com",
+    projectId: "youtubek-189316",
+    storageBucket: "youtubek-189316.appspot.com",
+    messagingSenderId: "657381104430"
+  }  
 };
